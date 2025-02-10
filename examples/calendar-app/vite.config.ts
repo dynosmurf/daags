2bf0ad619d18f6fc3a6728a9ce8fab3157209102
Ryan Murphy <ryan@devmurphy.com>
2025-02-10 11:13:24 -0700

    Fix dev time module resolution for cal example

--- examples/calendar-app/vite.config.ts
+++ examples/calendar-app/vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react' // Vite plugin for React
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist' // Where the built files will go
+  },
+  resolve: {
+    alias: {
+      '@daags/visualizer': path.resolve(
+        __dirname,
+        '../../packages/visualizer/src'
+      )
+    }
   }
 })
